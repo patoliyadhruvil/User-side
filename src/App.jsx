29636a020af15261f362bb361c175ea681ac5b1c
Dhruvil Patoliya <123619328+patoliyadhruvil@users.noreply.github.com>
2024-12-05 02:57:04 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+import AddItemsCart from './Component/AddItemsCart/AddItemsCart'
+import BurgerList from './Component/BurgerList/BurgerList'
+import Cart from './Component/Cart/Cart'
+import Categories from './Component/Categories/Categories'
+import ItemDetails from './Component/ItemDetails/IteamDetails'
+import Login from './Component/Login/Login'
+import Menu from './Component/Menu/Menu'
+import Payment from './Component/Payment/Payment'
+import SearchResults from './Component/SearchResults/SearchResults'
+import Search from './Component/SearchResults/SearchResults'
+import TrendingMenu from './Component/Trending-Menu/TrendingMenu'
+
+function App() {
+
+
+  return (
+    <>
+      {/* <Login /> */}
+      {/* <Menu /> */}
+      {/* <TrendingMenu/> */}
+      {/* <Categories /> */}
+      {/* <SearchResults /> */}
+      {/* <BurgerList/> */}
+      {/* <ItemDetails /> */}
+      {/* <Cart/> */}
+      {/* <Payment/> */}
+      <AddItemsCart/>
+    </>
+  )
+}
+
+export default App;
